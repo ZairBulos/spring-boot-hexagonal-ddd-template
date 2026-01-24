d98e1009ece1ce0d1983ac1376f85b939c6a1316
Zair Bulos <buloszair@gmail.com>
2026-01-24 19:09:03 -0300

    feat: simplify domain events handling in AggregateRoot

--- src/main/java/com/zair/domain/model/aggregates/AggregateRoot.java
+++ src/main/java/com/zair/domain/model/aggregates/AggregateRoot.java
@@ -3,21 +3,18 @@ package com.zair.domain.model.aggregates;
 import com.zair.domain.events.DomainEvent;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public abstract class AggregateRoot {
     private final List<DomainEvent> events = new ArrayList<>();
 
-    protected void registerEvent(DomainEvent event) {
+    protected void addEvent(DomainEvent event) {
         events.add(event);
     }
 
-    public List<DomainEvent> getEvents() {
-        return Collections.unmodifiableList(events);
-    }
-
-    public void clearEvents() {
+    public List<DomainEvent> pullEvents() {
+        List<DomainEvent> domainEvents = new ArrayList<>(events);
         events.clear();
+        return domainEvents;
     }
 }
